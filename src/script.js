1b6f8f50ecee1509064bae63f9e2812b2ba2bbfa
Jada Baron <jadachantellebaron@gmail.com>
2025-05-04 22:43:04 +0100

    Add button event listener to create grid with user prompt
    Set fixed container size and ensured container is cleared each time once user clicks button to create grid along with adding input validation to form max 1000 square grid.

--- src/script.js
+++ src/script.js
@@ -1,27 +1,44 @@
 const createGridButton = document.querySelector("#create-grid-button");
-console.log(createGridButton);
+
+createGridButton.addEventListener("click", createGrid);
 
 function createGrid() {
-  for (let i = 0; i < 100; i++) {
-    const container = document.querySelector(".container");
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+
+  let userNumInput = parseInt(
+    prompt(
+      "Please select a number between 1 and 100 to determine how big the grid will be:"
+    )
+  );
+
+  if (isNaN(userNumInput) || userNumInput < 1 || userNumInput > 100) {
+    alert("Please enter a valid number between 1 and 100");
+    return;
+  }
+
+  const containerSize = 600;
+  const squareSize = containerSize / userNumInput;
+
+  container.style.display = "flex";
+  container.style.width = `${containerSize}px`;
+  container.style.height = `${containerSize}px`;
+  container.style.flexWrap = "wrap";
+  container.style.margin = "0 auto";
+
+  let gridDimensions = Math.pow(userNumInput, 2);
+
+  for (let i = 0; i < gridDimensions; i++) {
     const gridSquare = document.createElement("div");
 
-    container.style.display = "flex";
-    container.style.width = "1000px";
-    container.style.flexWrap = "wrap";
-    container.style.margin = "auto";
-
-    gridSquare.style.display = "flex";
-    gridSquare.style.height = "100px";
-    gridSquare.style.width = "100px";
+    gridSquare.style.height = `${squareSize}px`;
+    gridSquare.style.width = `${squareSize}px`;
     gridSquare.style.border = "1px solid black";
-
-    container.append(gridSquare);
 
     gridSquare.addEventListener("mouseover", (event) => {
       gridSquare.classList.toggle("grid-square-hover-state");
     });
+
+    container.append(gridSquare);
   }
 }
-
-createGrid();
